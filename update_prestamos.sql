110380e896f9d2a97a5db15f767db6e066a6548e
SofiaTecumH <sofiatecumhernandez@gmail.com>
2025-04-01 23:44:13 -0600

    update_prestamos.sql

--- update_prestamos.sql
+++ update_prestamos.sql
@@ -73,3 +73,93 @@ depositos DECIMAL(10,2) DEFAULT '0.00',
 intereses DECIMAL(10,2) DEFAULT '0.00',
 saldo DECIMAL(10,2) DEFAULT '0.00'
 );
+
+-- ----------------------------------------------------------------------------
+-- Actualización tablas estudio socioeconómico
+-- ----------------------------------------------------------------------------
+
+ALTER TABLE vehiculos
+DROP COLUMN id_cliente,
+ADD COLUMN tipo_vehiculo VARCHAR(20) NOT NULL, -- Van, camioneta, bus, panel, etc
+ADD COLUMN finalidad_vehiculo ENUM ('USO_PERSONAL', 'USO_COMERCIAL') NOT NULL,
+ADD COLUMN cantidad_vehiculos INT NOT NULL,
+DROP COLUMN financiado, 
+ADD COLUMN placas VARCHAR(10) NOT NULL,
+ADD COLUMN fase ENUM ('PAGADO', 'EN_PROCESO_DE_PAGO') NOT NULL,
+ADD COLUMN fecha_compra DATE,
+ADD COLUMN estado_vehiculo ENUM ('USADO', 'NUEVO');
+
+ALTER TABLE referencias_personales
+DROP COLUMN id_cliente,
+ADD COLUMN `primer_nombre` VARCHAR(200) NOT NULL,
+ADD COLUMN `segundo_nombre` VARCHAR(200) NULL DEFAULT NULL,
+ADD COLUMN `otros_nombres` VARCHAR(200) NULL DEFAULT NULL,
+ADD COLUMN `primer_apellido` VARCHAR(250) NOT NULL,
+ADD COLUMN `segundo_apellido` VARCHAR(250) NULL DEFAULT NULL,
+ADD COLUMN `otros_apellidos` VARCHAR(100) NULL DEFAULT NULL,
+ADD COLUMN `departamento` VARCHAR(300) NOT NULL,
+ADD COLUMN `municipio` VARCHAR(300) NOT NULL,
+ADD COLUMN `zona` VARCHAR(300) NOT NULL,
+ADD COLUMN `grupo_habitacional` VARCHAR(300) NOT NULL,
+ADD COLUMN `vialidad` VARCHAR(300) NOT NULL,
+ADD COLUMN `numero_casa` VARCHAR(300) NOT NULL,
+ADD COLUMN  `DPI` VARCHAR(20) NOT NULL;
+
+ALTER TABLE otros_ingresos
+DROP COLUMN id_cliente,
+DROP COLUMN fuente,
+ADD COLUMN tipo_ingreso VARCHAR(100) NOT NULL, -- negocio, remesa, etc
+DROP COLUMN monto,
+ADD COLUMN monto_promedio_mensual DECIMAL(12,2) DEFAULT '0.00';
+
+ALTER TABLE ingresos_familiares
+DROP COLUMN id_cliente,
+ADD COLUMN cantidad_familiares INT NULL DEFAULT NULL,
+ADD COLUMN parentesco VARCHAR(20),
+DROP COLUMN monto,
+ADD COLUMN promedio_mensual DECIMAL(12,2) DEFAULT '0.00';
+
+RENAME TABLE empleo TO ocupacion;
+
+ALTER TABLE ocupacion
+DROP COLUMN id_cliente,
+DROP COLUMN antiguedad,
+ADD COLUMN fecha_inicio_labores DATE NULL DEFAULT NULL,
+DROP COLUMN salario,
+ADD COLUMN ingresos_base DECIMAL (12,2) DEFAULT '0.00';
+
+ALTER TABLE bienes_inmuebles
+DROP COLUMN id_cliente,
+DROP COLUMN hipoteca;
+
+ALTER TABLE historial_crediticio
+DROP COLUMN id_cliente;
+
+RENAME TABLE informacion_solicitante_credito TO solicitud_credito;
+
+ALTER TABLE solicitud_credito
+DROP PRIMARY KEY,
+CHANGE `id_solicitante` `id_solicitud` INT NOT NULL AUTO_INCREMENT,
+ADD PRIMARY KEY (id_solicitud),
+CHANGE `nombre` `primer_nombre` VARCHAR(100) NOT NULL,
+ADD COLUMN `segundo_nombre` VARCHAR(200) NULL DEFAULT NULL,
+ADD COLUMN `otros_nombres` VARCHAR(200) NULL DEFAULT NULL,
+ADD COLUMN `primer_apellido` VARCHAR(250) NOT NULL,
+ADD COLUMN `segundo_apellido` VARCHAR(250) NULL DEFAULT NULL,
+ADD COLUMN `otros_apellidos` VARCHAR(100) NULL DEFAULT NULL,
+DROP COLUMN direccion,
+ADD COLUMN `departamento` VARCHAR(300) NOT NULL,
+ADD COLUMN `municipio` VARCHAR(300) NOT NULL,
+ADD COLUMN `zona` VARCHAR(300) NOT NULL,
+ADD COLUMN `grupo_habitacional` VARCHAR(300) NOT NULL,
+ADD COLUMN `vialidad` VARCHAR(300) NOT NULL,
+ADD COLUMN `numero_casa` VARCHAR(300) NOT NULL,
+ADD COLUMN  `DPI` VARCHAR(20) NOT NULL,
+DROP COLUMN ingresos_promedio,
+DROP COLUMN historial_crediticio,
+DROP COLUMN bienes_inmuebles,
+DROP COLUMN referencias_personales,
+DROP COLUMN otros_datos;
+
+ALTER TABLE solicitud_credito DROP FOREIGN KEY fk_informacion_solicitante_credito_usuarios1;
+ALTER TABLE solicitud_credito DROP COLUMN usuarios_id_usuario;
